a6eef5b5faa1e4cf5eb55b46805f5fbcb8dc9d42
Samuel Heer <samuel@ex-akt.de>
2023-06-23 14:14:04 +0200

    provide framweworkpath

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 /*
- * This file is part of [package name].
+ * This file is part of ex-akt/contao-webpack-config-bundle.
  *
- * (c) John Doe
+ * (c) ex-akt.de
  *
  * @license LGPL-3.0-or-later
  */
@@ -17,7 +17,6 @@ use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use Exakt\ContaoWebpackConfigBundle\ContaoWebpackConfigBundle;
-use Symfony\Component\Config\Loader\LoaderInterface;
 
 class Plugin implements BundlePluginInterface
 {
@@ -27,13 +26,5 @@ class Plugin implements BundlePluginInterface
             BundleConfig::create(ContaoWebpackConfigBundle::class)
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
-    }
-
-     /**
-     * {@inheritdoc}
-     */
-    public function registerContainerConfiguration(LoaderInterface $loader, array $config)
-    {
-        $loader->load(__DIR__.'/../../config/config.yml');
     }
 }
--- src/DependencyInjection/ContaoWebpackConfigExtension.php
+++ src/DependencyInjection/ContaoWebpackConfigExtension.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 /*
- * This file is part of [package name].
+ * This file is part of ex-akt/contao-webpack-config-bundle.
  *
- * (c) John Doe
+ * (c) ex-akt.de
  *
  * @license LGPL-3.0-or-later
  */
@@ -15,13 +15,24 @@ namespace Exakt\ContaoWebpackConfigBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class ContaoWebpackConfigExtension extends Extension
+class ContaoWebpackConfigExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         $loader->load('services.yml');
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $frameworkConfig = $container->getExtensionConfig('framework');
+        $frameworkConfig['assets']['json_manifest_path'] = '%kernel.project_dir%/public/layout/manifest.json';
+
+        $container->prependExtensionConfig('framework', [
+            'assets' => $frameworkConfig['assets'],
+        ]);
     }
 }
