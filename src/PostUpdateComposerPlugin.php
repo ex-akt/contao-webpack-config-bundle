fe54f3878b627671aaa53fc8f2ee1b758aedc144
Samuel Heer <samuel@ex-akt.de>
2024-07-16 11:15:55 +0200

    test composer plugin

---
+++ src/PostUpdateComposerPlugin.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of ex-akt/contao-webpack-config-bundle.
+ *
+ * (c) ex-akt.de
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Exakt\ContaoWebpackConfigBundle;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+
+class PostUpdateComposerPlugin implements PluginInterface, EventSubscriberInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        // Aktivierungslogik
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'post-install-cmd' => 'onPostInstallUpdate',
+            'post-update-cmd' => 'onPostInstallUpdate',
+        ];
+    }
+
+    public function onPostInstallUpdate(Event $event)
+    {
+        $io = $event->getIO();
+        $io->write("Running custom script from plugin.");
+        // Füge hier deinen Code hinzu
+    }
+}
